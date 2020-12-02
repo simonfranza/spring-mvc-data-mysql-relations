0e56cbe7230044dd5d1148cce9ab4739f5cd280f
DOVENE AGOGUE <adovene@gmail.com>
2020-12-02 11:05:24 +0100

    initial commit

---
+++ src/main/resources/SpringData.sql
@@ -0,0 +1,105 @@
+-- MySQL dump 10.13  Distrib 8.0.21, for macos10.15 (x86_64)
+--
+-- Host: localhost    Database: spring
+-- ------------------------------------------------------
+-- Server version	8.0.21
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `Booking`
+--
+
+DROP TABLE IF EXISTS `Booking`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Booking` (
+  `idBooking` int NOT NULL AUTO_INCREMENT,
+  `customerFullName` varchar(45) DEFAULT NULL,
+  `tripCity` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`idBooking`),
+  KEY `tripCity_idx` (`tripCity`),
+  CONSTRAINT `tripCity` FOREIGN KEY (`tripCity`) REFERENCES `Trip` (`city`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Booking`
+--
+
+LOCK TABLES `Booking` WRITE;
+/*!40000 ALTER TABLE `Booking` DISABLE KEYS */;
+INSERT INTO `Booking` VALUES (7,'DAVID NIC','Germany'),(10,'DAVID NIC','Pat'),(11,'VILA NOM','Strasbourg'),(12,'ME ME','Strasbourg'),(13,'DUPONT','Lille'),(14,'CLEMENT MO','Lille');
+/*!40000 ALTER TABLE `Booking` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Trip`
+--
+
+DROP TABLE IF EXISTS `Trip`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Trip` (
+  `city` varchar(45) NOT NULL,
+  `price` varchar(45) DEFAULT NULL,
+  `date` datetime DEFAULT NULL,
+  PRIMARY KEY (`city`),
+  UNIQUE KEY `city_UNIQUE` (`city`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Trip`
+--
+
+LOCK TABLES `Trip` WRITE;
+/*!40000 ALTER TABLE `Trip` DISABLE KEYS */;
+INSERT INTO `Trip` VALUES ('Germany','1000.0','1999-12-31 23:00:00'),('Lille','300.0','2020-05-11 22:00:00'),('Pat','20.0','1222-12-11 23:00:00'),('Strasbourg','10.0','2020-06-10 22:00:00');
+/*!40000 ALTER TABLE `Trip` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `User`
+--
+
+DROP TABLE IF EXISTS `User`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `User` (
+  `password` varchar(45) DEFAULT NULL,
+  `login` varchar(45) NOT NULL,
+  `enabled` tinyint(1) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`login`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `User`
+--
+
+LOCK TABLES `User` WRITE;
+/*!40000 ALTER TABLE `User` DISABLE KEYS */;
+INSERT INTO `User` VALUES ('dov','dov',1),('spring','spring',1);
+/*!40000 ALTER TABLE `User` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-12-02 11:04:04
